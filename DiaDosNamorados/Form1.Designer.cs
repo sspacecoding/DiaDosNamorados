5bf58756f27ad5185a180f5daecb304734634cbe
giulia.bordignon <giulia.bordignon@terceiroslinx.com.br>
2024-06-09 00:26:36 -0300

    Adicionar arquivos de projeto.

---
+++ DiaDosNamorados/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DiaDosNamorados
+{
+    partial class PedidoDeNamoroForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelQuestion.Location = new System.Drawing.Point(32, 26);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(277, 31);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "Tu quer namorar comigo?";
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(32, 80);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(108, 43);
+            this.buttonYes.TabIndex = 1;
+            this.buttonYes.Text = "Sim";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(201, 80);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(108, 43);
+            this.buttonNo.TabIndex = 2;
+            this.buttonNo.Text = "Não";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(375, 167);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Pergunta Importante";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Button buttonNo;
+    }
+}
